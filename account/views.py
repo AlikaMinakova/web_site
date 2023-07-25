28655bd4664831cce617e15d2e8164c3e59af0ed
AlinaMinakova <94383135+AlinaMinackova@users.noreply.github.com>
2023-07-25 16:05:39 +0300

    add_profile_and_changeprofile

--- account/views.py
+++ account/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, \
+    UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 @login_required
@@ -10,6 +12,7 @@ def dashboard(request):
     return render(request,
                   'account/dashboard.html',
                   {'section': 'dashboard'})
+
 
 #
 # def user_login(request):
@@ -45,11 +48,35 @@ def register(request):
                 user_form.cleaned_data['password'])
             # Сохранить объект User
             new_user.save()
+            # Создать профиль пользователя
+            Profile.objects.create(user=new_user)
             return render(request,
-                    'account/template/register_done.html',
-                    {'new_user': new_user})
+                          'account/template/register_done.html',
+                          {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request,
                   'account/template/register.html',
                   {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(
+                                        instance=request.user.profile,
+                                        data=request.POST,
+                                        files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile)
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -34,6 +34,9 @@ LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'django.contrib.auth',
